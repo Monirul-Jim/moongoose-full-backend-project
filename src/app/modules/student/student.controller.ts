2d981daf0eb5c346fecd0c0b82bdc465c68fbbca
unknown <Monirul-Jim>
2024-06-03 14:04:14 +0600

    implement catchasync

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
 import { StudentServices } from './student.service';
 import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
 // const createStudent = async (req: Request, res: Response) => {
 //   try {
@@ -26,84 +26,73 @@ import sendResponse from '../../utils/sendResponse';
 //     });
 //   }
 // };
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getAllStudentFromDB();
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student are retrieve successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student are retrieve successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.getSingleStudentFromDB(studentId);
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Single Student are retrieve successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Single Student are retrieve successfully',
-      data: result,
-    });
-  } catch (err) {
-    // res.status(500).json({
-    //   success: false,
-    //   message: err.message || 'something went wrong',
-    //   error: err,
-    // });
-    next(err);
-  }
-};
-const deleteStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params;
-    const result = await StudentServices.deleteStudentFromDB(studentId);
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student deleted successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student deleted successfully',
-      data: result,
-    });
-  } catch (err) {
-    // res.status(500).json({
-    //   success: false,
-    //   message: err.message || 'something went wrong',
-    //   error: err,
-    // });
-    next(err);
-  }
-};
+
+const getAllStudents = catchAsync(async (req, res) => {
+  // try {
+  const result = await StudentServices.getAllStudentFromDB();
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student are retrieve successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student are retrieve successfully',
+    data: result,
+  });
+  // } catch (err) {
+  //   next(err);
+  // }
+});
+const getSingleStudent = catchAsync(async (req, res) => {
+  // try {
+  const { studentId } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(studentId);
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Single Student are retrieve successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Single Student are retrieve successfully',
+    data: result,
+  });
+  // } catch (err) {
+  //   // res.status(500).json({
+  //   //   success: false,
+  //   //   message: err.message || 'something went wrong',
+  //   //   error: err,
+  //   // });
+  //   next(err);
+  // }
+});
+const deleteStudent = catchAsync(async (req, res) => {
+  // try {
+  const { studentId } = req.params;
+  const result = await StudentServices.deleteStudentFromDB(studentId);
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student deleted successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student deleted successfully',
+    data: result,
+  });
+  // } catch (err) {
+  //   // res.status(500).json({
+  //   //   success: false,
+  //   //   message: err.message || 'something went wrong',
+  //   //   error: err,
+  //   // });
+  //   next(err);
+  // }
+});
 export const StudentController = {
   // createStudent,
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,42 +1,39 @@
-import { NextFunction, Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import { UserService } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: studentData } = req.body;
-    // data validation using zod
-    // const zodParseData = studentValidationSchema.parse(studentData);
+const createStudent = catchAsync(async (req, res) => {
+  // try {
+  const { password, student: studentData } = req.body;
+  // data validation using zod
+  // const zodParseData = studentValidationSchema.parse(studentData);
 
-    //will call service func to send this data
-    const result = await UserService.createStudentIntoDB(password, studentData);
+  //will call service func to send this data
+  const result = await UserService.createStudentIntoDB(password, studentData);
 
-    // send response
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is created successfully',
-    //   data: result,
-    // });
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (err) {
-    // res.status(500).json({
-    //   success: false,
-    //   message: err.message || 'something went wrong',
-    //   error: err,
-    // });
-    next(err);
-  }
-};
+  // send response
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'Student is created successfully',
+  //   data: result,
+  // });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student is created successfully',
+    data: result,
+  });
+  // } catch (err) {
+  //   // res.status(500).json({
+  //   //   success: false,
+  //   //   message: err.message || 'something went wrong',
+  //   //   error: err,
+  //   // });
+  //   next(err);
+  // }
+});
 export const UserController = {
   createStudent,
 };
